4054996eaeda3450ff5cb977e77299c9411d2fbc
ASTOR FEDERICI <48794543@ort>
2025-04-23 08:22:34 -0300

    Terminado

--- TP1React_Federici/src/App.jsx
+++ TP1React_Federici/src/App.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import './index.css';
+import './App.css';
 import Form from './assets/componentes/formulario';
 import Cita from './assets/componentes/cita';
 
 function App() {
   return (
-    <div>
-      <h1>ADMINISTRADOR DE PACIENTES</h1>
-      <div className="contenedor">
-        <div className="column">
-          <h2>CREAR MI CITA</h2>
-          <Form />
-        </div>
-        <div className="column">
-          <h2>ADMINISTRA TUS CITAS</h2>
-          <Cita />
-        </div>
+  <div>
+    <h1>ADMINISTRADOR DE PACIENTES</h1>
+    <div className="contenedor-flex">
+      <div className="columna">
+      <h2>CREAR MI CITA</h2>
+        <Form />
+      </div>
+      <div className="columna">
+      <h2>ADMINISTRA TUS CITAS</h2>
+        <Cita />
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default App;
